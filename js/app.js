06c6d09a6ede20902703101e707804fca229d89d
RocioMartinez <rym5411@gmail.com>
2023-04-29 20:15:07 -0400

    added image, and edited js

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ alert(`I'm glad to have you here ${userName}. I want you to get to know me, thro
 let firstGuess = prompt('Do I have any pets?').toLowerCase();
 
 if (firstGuess === 'yes' || firstGuess === 'y'){
+  //console.log('wrong user response');
   alert('That is incorrect, but I would love to get one!');
 } else if(firstGuess === 'no' || firstGuess === 'n'){
   alert('That\'s right! and sad, I know.');
@@ -16,14 +17,16 @@ if (firstGuess === 'yes' || firstGuess === 'y'){
 let secondGuess = prompt('Is my favorite color pink?').toLowerCase();
 
 if (secondGuess === 'yes' || secondGuess === 'y'){
+  //console.log('user is wrong');
   alert('You would be wrong. My favorite color is emerald green.');
 } else if(secondGuess === 'no' || secondGuess === 'n'){
-  alert('That\s right! Nothing against pink, I just love emerald green.');
+  alert('That\'s right! Nothing against pink, I just love emerald green.');
 }
 
 let thirdGuess = prompt('Do I like to workout?').toLowerCase();
 
 if (thirdGuess === 'yes' || thirdGuess === 'y'){
+  //console.log('user is right');
   alert('That is correct! Usually 5 days a week to keep my stress down.');
 } else if(thirdGuess === 'no' || thirdGuess === 'n'){
   alert('Wrong, I am a nightmare if I dont workout!');
@@ -32,6 +35,7 @@ if (thirdGuess === 'yes' || thirdGuess === 'y'){
 let fourthGuess = prompt('Do I enjoy reading?').toLowerCase();
 
 if (fourthGuess === 'yes' || fourthGuess === 'y'){
+  //console.log('correct answer');
   alert('Correct! I love to read, it\'s a good escape');
 } else if(fourthGuess === 'no' || fourthGuess === 'n'){
   alert('That would be wrong.');
@@ -40,7 +44,8 @@ if (fourthGuess === 'yes' || fourthGuess === 'y'){
 let fithGuess = prompt('Do I like to cook?').toLowerCase();
 
 if (fithGuess === 'yes' || fithGuess === 'y'){
-  alert(`That\'s right! I challenge myself to make healthy versions of Mexican food. Thanks for playing ${userName}!`);
+  //console.log('right answer');
+  alert(`That's right! I challenge myself to make healthy versions of Mexican food. Thanks for playing ${userName}!`);
 } else if (fithGuess === 'no' || fithGuess === 'n'){
   alert(`Incorrect, I try to make my workouts easier by cooking healthy food. Thanks for playing ${userName}`);
 }
